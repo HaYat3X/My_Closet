a6ea3b3b45f89648b95471e6aff57be93bb57ea4
Hayate_T <hayatesato1@gmail.com>
2023-05-10 00:18:44 +0900

    javascriptの導入、リアルタイムバリデーションの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,29 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// 無効なフィールドがある場合にフォーム送信を無効にするスターターJavaScriptの例
+(() => {
+    'use strict';
 
-Rails.start()
-Turbolinks.start()
+    // Bootstrapカスタム検証スタイルを適用してすべてのフォームを取得
+    const forms = document.querySelectorAll('.needs-validation');
+
+    // ループして帰順を防ぐ
+    Array.from(forms).forEach((form) => {
+        form.addEventListener(
+            'submit',
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            },
+            false
+        );
+    });
+})();
+
+Rails.start();
+Turbolinks.start();
