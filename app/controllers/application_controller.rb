19062d968d95bbadf336d9dda90baabc541ea9a9
Hayate_T <hayatesato1@gmail.com>
2023-05-09 10:32:18 +0900

    前回の作業範囲まで修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # ! user_nameを登録時に登録させるための設定
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    # ! user_nameを登録時に登録させるための設定
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    end
 end
