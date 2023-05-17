f3568407913380c2c25ccfea98de02718f9ff70d
Hayate_T <hayatesato1@gmail.com>
2023-05-17 09:42:00 +0900

    sns一覧ページ、sns詳細ページの作成

--- app/controllers/sns/posts_controller.rb
+++ app/controllers/sns/posts_controller.rb
@@ -1,15 +1,15 @@
 class Sns::PostsController < ApplicationController
     # ! 一覧取得メソッド
     def list
-        @snss = Social.all
+        @snss = Social.all.page(params[:page]).per(40)
     end    
 
     # ! 詳細取得メソッド
     def show
         # * 詳細情報を一件取得する
-        @sns = Social.find(1)
+        @sns = Social.find(5)
 
-        # * item1〜6の情報を取得する
+        # * item1〜6のアイテム情報をクローゼットテーブルから取得する
         @item1 = Closet.find_by(id: @sns.item1)
         @item2 = Closet.find_by(id: @sns.item2)
         @item3 = Closet.find_by(id: @sns.item3)
