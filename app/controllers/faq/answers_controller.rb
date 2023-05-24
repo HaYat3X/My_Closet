9285b39f6c84b1f0bd0281e73e784d87ed7706c3
重山大輝 <nuuutaro@gmail.com>
2023-05-24 09:52:15 +0900

    Q&A回答削除

--- app/controllers/faq/answers_controller.rb
+++ app/controllers/faq/answers_controller.rb
@@ -21,7 +21,23 @@ class Faq::AnswersController < ApplicationController
         end
     end
 
-    # ! 編集
+    # ! 削除
+    def delete
+        # * urlから投稿id取得
+        @answer = Answer.find(params[:id])
+
+        #ユーザーIDが自分のではなかった場合、他のユーザーIDから削除できないようにする。
+        if @answer.user_id != current_user.id
+            redirect_to "/", alert: "不正なアクセスが行われました。"
+        end
+    
+        # 削除
+        if @answer.destroy
+            redirect_to "/question/list", notice: "投稿を削除しました"
+        else
+            redirect_to"/question/list", alert: "投稿の編集に削除しました"
+        end
+    end
 
     # ! 関数
     private
