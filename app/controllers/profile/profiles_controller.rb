78ea641a41755aa59dc4b30dcd7407c8fbe9cd55
矢田桂都 <yatakeito@makkubukkuea.local>
2023-05-26 12:40:06 +0900

    プロフィール作成(いいねした投稿以外)

--- app/controllers/profile/profiles_controller.rb
+++ app/controllers/profile/profiles_controller.rb
@@ -1,6 +1,17 @@
 class Profile::ProfilesController < ApplicationController
     # ! ログイン中のユーザを取得
     def show
-        
+        # ログイン中のユーザ情報
+        @user = current_user 
+
+        # ログイン中のユーザが投稿したSNS投稿
+        @snss = Social.where(user_id:@user.id)
+
+        #ログイン中のユーザーがいいねしたSNS投稿
+        @sns_likes = SocialLike.where(user_id: @user.id)
+     
+
+        #ログイン中のユーザーが投稿したクローゼット
+        @closets = Closet.where(user_id:@user.id)
     end    
 end
