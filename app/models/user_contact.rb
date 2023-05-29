5eb306ebbbded6c08e3c606974067d0a4805d14f
Hayate_T <hayatesato1@gmail.com>
2023-05-29 19:53:09 +0900

    contactコントローラー、contactテーブルの作成

---
+++ app/models/user_contact.rb
@@ -0,0 +1,2 @@
+class UserContact < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_26_062256) do
+ActiveRecord::Schema.define(version: 2023_05_29_104704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,15 @@ ActiveRecord::Schema.define(version: 2023_05_26_062256) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_suggests_on_user_id"
+  end
+
+  create_table "user_contacts", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "content", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "user_relations", force: :cascade do |t|
