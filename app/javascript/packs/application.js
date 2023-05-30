0b37b49e78dda77c8151fb98302990024504c01e
Hayate_T <hayatesato1@gmail.com>
2023-05-30 23:28:35 +0900

    ユーザ側のサインインページのUIをカスタマイズ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,29 +1,24 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 
-// 無効なフィールドがある場合にフォーム送信を無効にするスターターJavaScriptの例
-(() => {
-    "use strict";
+// ! フォームのバリデーションをチェックする （全フォーム共通）
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-    // Bootstrapカスタム検証スタイルを適用してすべてのフォームを取得
-    const forms = document.querySelectorAll(".needs-validation");
+    // * Bootstrapカスタム検証スタイルを適用してすべてのフォームを取得
+    const forms = document.querySelectorAll('.needs-validation');
 
-    // ループして帰順を防ぐ
+    // * ループして帰順を防ぐ
     Array.from(forms).forEach((form) => {
-        form.addEventListener(
-            "submit",
-            (event) => {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-
-                form.classList.add("was-validated");
-            },
-            false
-        );
+        form.addEventListener('submit', (event) => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
     });
-})();
+});
 
 Rails.start();
 Turbolinks.start();
