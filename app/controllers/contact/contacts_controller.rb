2d3f49caffd8600becc895efe8b62e9e93d5ddaf
Hayate_T <hayatesato1@gmail.com>
2023-05-30 10:08:50 +0900

    お問い合わせ時にバインドするストロングパラメータの追加

--- app/controllers/contact/contacts_controller.rb
+++ app/controllers/contact/contacts_controller.rb
@@ -1,5 +1,19 @@
 class Contact::ContactsController < ApplicationController
+    # * お問い合わせフォーム
     def new
         @contact = UserContact.new
+    end
+
+    # * お問い合わせ処理
+    def create
+        
+    end
+
+    private
+
+    # ! 投稿時にバインドするパラメータ
+    def posts_params
+        # * Closetモデルにバインドする
+        params.require(:user_contact).permit(:email, :name, :content)
     end
 end
