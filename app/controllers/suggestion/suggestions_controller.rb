216f976a37e8c8de45631916506a609bb7b110ac
Hayate_T <hayatesato1@gmail.com>
2023-06-03 15:42:00 +0900

    クローゼットからコーデを提案する機能の技術検証、検証OK

--- app/controllers/suggestion/suggestions_controller.rb
+++ app/controllers/suggestion/suggestions_controller.rb
@@ -18,5 +18,32 @@ class Suggestion::SuggestionsController < ApplicationController
             # * おすすめのユーザを出力
             @users = User.where("tendency LIKE ? OR tendency LIKE ?", key_word1, key_word2).order("RANDOM()").limit(10)
         end
+
+        # ! --------------追加機能の検証---------------
+        # * 現在の日時から"月"を取得
+        current_month = Time.now.month
+
+        # * 作成したルールの読み込み
+        if current_month = 4 or 5 or 6 or 7
+            # ? 春のルールをランダムに取得
+            random_number = rand(1..2).to_s
+            rule = SPRING_RULES['rule_' + random_number]
+
+            @outfit = []
+
+            outer = Closet.where(big_Category: "アウター", user_id: current_user.id, color: rule['outer_color']).order("RANDOM()").first
+            tops = Closet.where(big_Category: "トップス", user_id: current_user.id, color: rule['top_color']).order("RANDOM()").first
+            pants = Closet.where(big_Category: "パンツ", user_id: current_user.id, color: rule['bottom_color']).order("RANDOM()").first
+
+            # ? からのデータがある場合は配列自体をカラにする
+            if outer.nil? || tops.nil? || pants.nil?
+                @message = "アイテム数が少ないです。各カテゴリーのアイテムを追加してください。"
+                @outfit = nil
+            else
+                @outfit << outer
+                @outfit << tops
+                @outfit << pants
+            end
+        end
     end
 end
---
+++ config/initializers/outfit_rules.rb
@@ -0,0 +1,28 @@
+# ! クローゼットから服を組み合わせて提案するためのルール
+# * 4月から7月 （春）のルール
+SPRING_RULES = {
+    'rule_1' => {
+        'outer_color' => 'black',
+        'top_color' => 'white',
+        'bottom_color' => 'blue',
+    },
+    'rule_2' => {
+        'outer_color' => 'blue',
+        'top_color' => 'white',
+        'bottom_color' => 'black',
+    }
+    # 他のルールを追加...
+}
+
+# * 8月から10月 （夏）のルール
+SUMMER_RULES = {
+    'rule_1' => {
+        'top_color' => 'white',
+        'bottom_color' => 'blue',
+    },
+    'rule_2' => {
+        'top_color' => 'white',
+        'bottom_color' => 'black',
+    }
+    # 他のルールを追加...
+}
