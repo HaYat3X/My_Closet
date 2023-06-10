908c315faa1bbdf17c990fd3c768a587cd0f433a
Hayate_T <hayatesato1@gmail.com>
2023-06-10 12:29:53 +0900

    運営からの

--- app/controllers/profile/alerts_controller.rb
+++ app/controllers/profile/alerts_controller.rb
@@ -1,4 +1,7 @@
 class Profile::AlertsController < ApplicationController
+    # ! ログインが必要ないメソッドを記述する (ログインが必要なメソッドは書かない)
+    before_action :move_to_signed_in, except: [:list, :show]
+
     # * お知らせを一覧取得
     def list
         @alerts = Alert.all()
@@ -8,4 +11,13 @@ class Profile::AlertsController < ApplicationController
     def show
         @alert = Alert.find(params[:id])
     end    
+
+    private
+
+    # ! ログインがしているのか判定する
+    def move_to_signed_in
+        unless user_signed_in?
+            redirect_to new_user_session_path, alert: "この操作は、サインインが必要です。"
+        end
+    end
 end
