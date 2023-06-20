d09c3c8f78abe01fc9b8f9eb81f55aa5b5907357
Hayate_T <hayatesato1@gmail.com>
2023-06-20 17:54:07 +0900

    本番環境への適応

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,5 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
-
 environment.plugins.append(
     'Provide',
     new webpack.ProvidePlugin({
@@ -9,5 +8,4 @@ environment.plugins.append(
         Popper: ['popper.js', 'default']
     })
 );
-
 module.exports = environment;
