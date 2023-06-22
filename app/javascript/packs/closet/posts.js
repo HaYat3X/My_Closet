8a089c24ffe0cf59926007137c3fff3e070b8e32
Hayate_T <hayatesato1@gmail.com>
2023-06-22 20:36:04 +0900

    クローゼットアイテム登録、編集画面で商カテゴリーに表示されるデフォルト値を追加

--- app/javascript/packs/closet/posts.js
+++ app/javascript/packs/closet/posts.js
@@ -2,21 +2,63 @@
 export function ClosetSelectValue() {
     $('#big-category-select').change(function() {
         var selectedValue = $(this).val();
+        var smallCategorySelect = $('#small-category-select');
 
         $.ajax({
             url: '/realtime_selected_value', // 適切なエンドポイントのURLを設定
             method: 'POST',
             data: { selected_value: selectedValue },
             success: function(response) {
-                // // ーバーサイドからのレスポンスを処理
-                var smallCategorySelect = $('#small-category-select');
                 smallCategorySelect.empty(); // 小カテゴリーセレクトボックスをクリア
                 var options = response.options;
-                for (var i = 0; i < options.length; i++) {
-                    var option = $('<option>').text(options[i]);
-                    smallCategorySelect.append(option); // 選択肢を追加
+                if (options) {
+                    for (var i = 0; i < options.length; i++) {
+                        var option = $('<option>').text(options[i]);
+                        smallCategorySelect.append(option); // 選択肢を追加
+                    }
+                } else {
+                    // デフォルトの値を追加する
+                    var default_options = [
+                        'ジャケット',
+                        'コート',
+                        'ピーコート',
+                        'ダウンジャケット',
+                        'レザージャケット',
+                        'ウインドブレーカー',
+                        'カーディガン',
+                        'Tシャツ',
+                        'シャツ',
+                        'ブラウス',
+                        'ポロシャツ',
+                        'ニットセーター',
+                        'パーカー',
+                        'ジーンズ',
+                        'パンツ',
+                        'ショートパンツ',
+                        'スカート',
+                        'レギンス',
+                        'ショーツ',
+                        'スカート',
+                        'スニーカー',
+                        'パンプス',
+                        'サンダル',
+                        'ブーツ',
+                        'フラットシューズ',
+                        '革靴',
+                        'ネックレス',
+                        'ブレスレット',
+                        'ピアス',
+                        'リング',
+                        'ヘアアクセサリー',
+                        'その他',
+                    ];
+                    for (var i = 0; i < default_options.length; i++) {
+                        var option = $('<option>').text(default_options[i]);
+                        smallCategorySelect.append(option); // 選択肢を追加
+                    }
                 }
             },
+
             error: function(error) {
                 // エラーハンドリング
                 console.log(error);
