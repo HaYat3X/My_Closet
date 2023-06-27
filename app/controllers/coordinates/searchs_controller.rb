b22ad7536db0097dfa414c28af0867d39108dc00
Hayate_T <hayatesato1@gmail.com>
2023-06-27 14:35:04 +0900

    coordinates/searchsコントローラーのリファクタリング

--- app/controllers/coordinates/searchs_controller.rb
+++ app/controllers/coordinates/searchs_controller.rb
@@ -2,17 +2,15 @@ class Coordinates::SearchsController < ApplicationController
     # ! ログインが必要ないメソッドを記述する (ログインが必要なメソッドは書かない)
     before_action :move_to_signed_in, except: []
 
-    # 検索用メソッド
+    # ! キーワードを元に、クローゼットテーブルの情報を検索するメソッド
     def search
-        # * フォームの値を受け取る
+        # * 検索されたキーワードの値を受け取る
         key_word = '%' + params[:search] + '%'
+
         # * 大文字、小文字を区別せずに検索
-        closet_table = Closet.arel_table
-        # * 検索
-        @search_result = Closet.where(closet_table[:search].matches(key_word))
+        @search_result = Closet.where("LOWER(search) LIKE ?", key_word.downcase)
     end
 
-    # ! (privateは外部クラスから参照できない)
     private
 
     # ! ログインがしているのか判定する
@@ -21,5 +19,4 @@ class Coordinates::SearchsController < ApplicationController
             redirect_to new_user_session_path, alert: "この操作は、サインインが必要です。"
         end
     end
-
 end
