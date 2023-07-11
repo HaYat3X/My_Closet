3b7c079a8eb11df36f647ae4346f0f7958966bb9
Hayate_T <hayatesato1@gmail.com>
2023-07-11 16:34:22 +0900

    クローゼット詳細画面に、類似したコーディネートを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
     end
+
+    # ! 正規表現の実装
 end
