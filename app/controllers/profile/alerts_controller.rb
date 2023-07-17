e1df9503765feef88b8c605c3ed9441d62e59724
Hayate_T <hayatesato1@gmail.com>
2023-07-17 21:50:58 +0900

    通知機能に既読、未読機能を追加

--- app/controllers/profile/alerts_controller.rb
+++ app/controllers/profile/alerts_controller.rb
@@ -4,11 +4,9 @@ class Profile::AlertsController < ApplicationController
 
     # * お知らせを一覧取得
     def list
-        @alerts = Alert.all().order(created_at: :desc)
+        @alerts = Alert.order(created_at: :desc)
         # * 自分に当てられた通知かつ、通知の作成元が自分以外の通知を取得
-        @notification = Notification.where(user_id: current_user.id).where.not(source_user_id: current_user.id)
-        # @notification = Notification.where(user_id: current_user.id)
-        pp @notification
+        @notification = Notification.where(user_id: current_user.id).where.not(source_user_id: current_user.id).order(created_at: :desc).order(read: :asc)
     end
     
     # * お知らせ詳細取得
@@ -22,12 +20,21 @@ class Profile::AlertsController < ApplicationController
             # * 通知を発生させたユーザー情報
             @source_user_id = User.find(@notification.source_user_id)
 
-            # * SNSいいね通知投稿
             if @notification.source_post_id != 0
-                @source_post_id = Social.find(@notification.source_post_id)
-
-                # * 回答
-                @source_answer_post_id = Question.find(@notification.source_post_id)
+                if @notification.notification_type === "like"
+                    # * SNSいいね通知投稿
+                    @source_post_id = Social.find(@notification.source_post_id)
+                else
+                    # * 回答
+                    @source_answer_post_id = Question.find(@notification.source_post_id)
+                end
+            end
+            
+            # * 通知を既読にする
+            if @notification.update(read: 1)
+                render :show
+            else
+                render :show
             end
         end        
     end    
