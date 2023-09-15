9d2540f9cd7b421b85a2c4fe9cddf12603199351
Hayate_T <hayatesato1@gmail.com>
2023-09-15 11:16:04 +0900

    画像アップロード時に画像最適化処理を追加

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -1,57 +1,33 @@
 class UserImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+    include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
     # * 本番環境とローカル環境でアップロードする環境を分ける
     if Rails.env.production?
-      # ? S3
-      storage :fog
+        # ? S3
+        storage :fog
     else
-      # ? LOCAL
-      storage :file
+        # ? LOCAL
+        storage :file
     end
 
     version :forced_size do
-      process resize_to_fill: [300, 300]
+        process resize_to_fill: [300, 300]
     end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+    process :optimize_image
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+	def optimize_image
+		manipulate! do |img|
+			img.strip
+			img.resize '250x250>'
+		end
+	end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+    def store_dir
+        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_allowlist
-    %w(jpg jpeg png)
-  end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+    def extension_allowlist
+        %w(jpg jpeg png)
+    end
 end
