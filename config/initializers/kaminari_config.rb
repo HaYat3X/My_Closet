1f43be890d7d9ed9d841d75871d9f800e70b1193
Hayate_T <hayatesato1@gmail.com>
2023-09-26 21:22:04 +0900

    コーディネート投稿、更新画面のボタンデザインを変更

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 3
+  config.default_per_page = 64
   config.max_per_page = 64
   config.window = 1
   # config.outer_window = 0
